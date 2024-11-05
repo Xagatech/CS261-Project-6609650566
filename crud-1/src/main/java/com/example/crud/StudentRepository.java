033c6f84d6d405ad025eb8e5736aba2065224de4
Peemapat Jiravatvanich-6609650566 <145252834+Xagatech@users.noreply.github.com>
2024-11-05 11:05:05 +0700

    Add files via upload

---
+++ crud-1/src/main/java/com/example/crud/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.example.crud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class StudentRepository {
+	
+}
